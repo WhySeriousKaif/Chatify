45ccc4e8696df01693109d2abeb693d0ebc8315d
Kaif <kaif00786001@gmail.com>
2025-10-09 20:23:52 +0530

    Add authentication pages and integrate routing with Zustand for state management

---
+++ frontend/src/pages/ChatPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ChatPage = () => {
+  return (
+    <div>
+      <h1>Chat Page</h1>
+      <button className='btn btn-primary'>Button</button>
+    </div>
+  )
+}
+
+export default ChatPage
