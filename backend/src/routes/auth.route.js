e519b5bf3054d3783fa33b4dac9ab2ae47cd8b5d
Kaif <kaif00786001@gmail.com>
2025-10-09 17:01:59 +0530

    Add Arcjet security middleware for bot detection and rate limiting; update auth routes to include protection

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,18 +1,24 @@
 import express, { Router } from "express";
-import dotenv from "dotenv";
-import { signUp,logIn ,logOut, updateProfile} from "../controller/auth.controller.js";
+import {
+  signUp,
+  logIn,
+  logOut,
+  updateProfile,
+} from "../controller/auth.controller.js";
 import { protectedRoute } from "../middlewares/auth.middleware.js";
-
-dotenv.config();
+import { arcjetProtection } from "../middlewares/arcjet.middleware.js";
 
 const authRoute = express.Router();
 
-authRoute.post("/login",logIn);
+authRoute.use(arcjetProtection);
+
+authRoute.post("/login", logIn);
 
 authRoute.post("/signup", signUp);
-authRoute.post('/logout', logOut);  
-authRoute.post('/update-profile',protectedRoute, updateProfile);
-authRoute.get('/check',protectedRoute,(req,res)=>res.status(200).json(req.user))
-
+authRoute.post("/logout", logOut);
+authRoute.post("/update-profile", protectedRoute, updateProfile);
+authRoute.get("/check", protectedRoute, (req, res) =>
+  res.status(200).json(req.user)
+);
 
 export default authRoute;
