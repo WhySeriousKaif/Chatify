52294bd64e1284f06d8616a03521c97be4f7f17f
Kaif <kaif00786001@gmail.com>
2025-10-25 21:21:32 +0530

    Revert to pink and cyan gradient background

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -40,8 +40,8 @@ function ChatContainer() {
       {/* Beautiful message background - same as landing page */}
       <div className="absolute inset-0 bg-slate-900" />
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
-      <div className="absolute top-0 -left-4 size-96 bg-emerald-500 opacity-20 blur-[100px]" />
-      <div className="absolute bottom-0 -right-4 size-96 bg-violet-500 opacity-20 blur-[100px]" />
+      <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
+      <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
       
       <div className="relative z-10 flex flex-col h-full">
         <ChatHeader />
